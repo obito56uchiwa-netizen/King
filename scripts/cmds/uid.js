fccd258c1233574ede4572af6970bd12779e5234
obito56uchiwa-netizen <obito56uchiwa@gmail.com>
2026-05-17 18:16:00 +0100

    Update uid.js

--- scripts/cmds/uid.js
+++ scripts/cmds/uid.js
@@ -1,63 +1,41 @@
-const { findUid } = global.utils;
-const regExCheckURL = /^(http|https):\/\/[^ "]+$/;
-
 module.exports = {
 	config: {
 		name: "uid",
-		version: "1.3",
-		author: "NTKhang",
-		countDown: 5,
+		aliases: ["id", "userid"],
+		version: "2.0",
 		role: 0,
-		description: {
-			vi: "Xem user id facebook của người dùng",
-			en: "View facebook user id of user"
-		},
+		description: "Get user UID + profile link",
 		category: "info",
-		guide: {
-			vi: "   {pn}: dùng để xem id facebook của bạn"
-				+ "\n   {pn} @tag: xem id facebook của những người được tag"
-				+ "\n   {pn} <link profile>: xem id facebook của link profile"
-				+ "\n   Phản hồi tin nhắn của người khác kèm lệnh để xem id facebook của họ",
-			en: "   {pn}: use to view your facebook user id"
-				+ "\n   {pn} @tag: view facebook user id of tagged people"
-				+ "\n   {pn} <profile link>: view facebook user id of profile link"
-				+ "\n   Reply to someone's message with the command to view their facebook user id"
-		}
+		author: "Stacks"
 	},
 
-	langs: {
-		vi: {
-			syntaxError: "Vui lòng tag người muốn xem uid hoặc để trống để xem uid của bản thân"
-		},
-		en: {
-			syntaxError: "Please tag the person you want to view uid or leave it blank to view your own uid"
-		}
-	},
+	onStart: async function ({ api, event, message }) {
 
-	onStart: async function ({ message, event, args, getLang }) {
-		if (event.messageReply)
-			return message.reply(event.messageReply.senderID);
-		if (!args[0])
-			return message.reply(event.senderID);
-		if (args[0].match(regExCheckURL)) {
-			let msg = '';
-			for (const link of args) {
-				try {
-					const uid = await findUid(link);
-					msg += `${link} => ${uid}\n`;
-				}
-				catch (e) {
-					msg += `${link} (ERROR) => ${e.message}\n`;
-				}
-			}
-			message.reply(msg);
-			return;
-		}
+		let target = event.messageReply
+			? event.messageReply.senderID
+			: event.senderID;
 
-		let msg = "";
-		const { mentions } = event;
-		for (const id in mentions)
-			msg += `${mentions[id].replace("@", "")}: ${id}\n`;
-		message.reply(msg || getLang("syntaxError"));
+		try {
+			const userInfo = await api.getUserInfo(target);
+			const name = userInfo[target].name || "Unknown";
+
+			const profileLink = `https://facebook.com/${target}`;
+
+			return message.reply(
+`🖤 𝙳𝙰𝚁𝙺 𝚂𝚃𝙰𝙲𝙺𝚂 UID
+
+👤 Name: ${name}
+🆔 UID: ${target}
+🔗 Profile: ${profileLink}
+
+💀 Status: ACTIVE NODE`
+			);
+
+		} catch (e) {
+			return message.reply(
+`🆔 UID: ${target}
+🔗 Profile: https://facebook.com/${target}`
+			);
+		}
 	}
 };
